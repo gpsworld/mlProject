d3074aa0596d16b44384753e1e8339eb30e24063
gpsworld <gpsworld@eng.rizvi.edu.in>
2024-03-08 19:46:34 +0530

    Logging and Exception

--- src/logger.py
+++ src/logger.py
@@ -1,28 +1,11 @@
-# import logging
-# import os
-# from datetime import datetime
-
-# LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M')}.log"
-# logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-# os.makedirs(logs_path, exist_ok=True)
-# LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
-# logging.basicConfig(
-#   filename=LOG_FILE_PATH,
-#   format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-
-#   level = logging.INFO, 
-# )
-
-# if __name__ == " __main__":
-#   logging.info("Loggin has started")
 import logging
 import os
-from datetime import datetime
+from datetime import datetime 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
+
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
@@ -30,4 +13,3 @@ logging.basicConfig(
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
