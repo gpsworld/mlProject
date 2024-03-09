39937da7e2dfcf57dfa0ed5d0f417f73c23ffbcf
gpsworld <gpsworld@eng.rizvi.edu.in>
2024-03-09 12:19:15 +0530

    Data Transformation done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data )
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
